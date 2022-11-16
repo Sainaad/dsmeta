66f9119d3bbce39370f4021ae777126a7617c3e3
Sainad <daniassillva225@gmail.com>
2022-11-15 22:08:13 -0400

    Use State

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header"
-import Salescard from "./components/Salescard/Index"
+import Header from "./components/Header";
+import Salescard from "./components/Salescard/Index";
 
 function App() {
   return (
@@ -16,4 +16,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
